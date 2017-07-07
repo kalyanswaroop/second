3deb105a4aa09c51fb00bd3722c2c5233dcfb583
kalyan's <kalyan's@192.168.1.9>
2017-07-07 17:24:16 +0530

    Commiting second repo

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+	echo "Hello World";
+	echo "<br> My first PHP program on eclipse";	
+
+	class Demo{
+		
+		public function one() {
+			return 1;
+		}
+		
+		public function two() {
+			return "A word";
+		}
+		
+	}
+?>
